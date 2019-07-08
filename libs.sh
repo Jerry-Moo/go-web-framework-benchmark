85fd15d2423f3ebaabae917c358cfea89e4a36f5
wuchujie <wuchujie@Mac-mini-2.local>
2019-07-08 09:58:05 +0800

    fix test web-framewrok

--- libs.sh
+++ libs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-web_frameworks=("default" "atreugo" "baa" "beego" "bone" "bxog" "chi" "denco")
+web_frameworks=("default" "atreugo" "baa" "beego" "bone" "bxog" "chi" "denco" "echov4" "fasthttp" "fasthttprouter" "fasthttp/router" "fasthttp-routing" "fastrouter" "fresh" "gear" "gin" "gocraftWeb" "goji" "gojsonrest" "golf" "gorestful" "gorilla" "gorouter" "go-ozzo" "gowww" "gramework" "httprouter" "httptreemux")
